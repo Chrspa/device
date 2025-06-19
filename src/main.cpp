b2334294c54e22a393cdbdc39fd71d7f97d49937
Leander <leander.gerwing@gmail.com>
2025-06-19 18:02:29 +0200

    fix: wrong setup, copy example from GxEPD2

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,46 @@
+#include <Arduino.h>
 #include <GxEPD2_BW.h>
-#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
 
 #define EPD_DIN    10
 #define EPD_CLK    9
 #define EPD_CS     8
 #define EPD_DC     7
 #define EPD_RST    6
-#define EPD_BUSY   5
+#define EPD_BUSY   /*-1*/5
 
 GxEPD2_BW<GxEPD2_290, GxEPD2_290::HEIGHT> display(
-    GxEPD2_290(/*CS=*/ EPD_CS, /*DC=*/ EPD_DC, /*RST=*/ EPD_RST, /*BUSY=*/ EPD_BUSY));
+    GxEPD2_290(EPD_CS, EPD_DC, EPD_RST, EPD_BUSY)
+);
+
+const char TEXT[] = "Hello world!";
 
 void setup() {
     Serial.begin(115200);
-    display.init();
+    Serial.println("Init display");
+
+    display.init(115200, true, 2, false);
     display.setRotation(1);
-    display.setTextSize(1);
-    display.setCursor(10, 10);
-    display.println("Hallo, ePaper!");
-    display.display();
+    display.setFont(&FreeMonoBold9pt7b);
+    display.setTextColor(GxEPD_BLACK);
+    int16_t tbx, tby; uint16_t tbw, tbh;
+    display.getTextBounds(TEXT, 0, 0, &tbx, &tby, &tbw, &tbh);
+    // center the bounding box by transposition of the origin:
+    uint16_t x = ((display.width() - tbw) / 2) - tbx;
+    uint16_t y = ((display.height() - tbh) / 2) - tby;
+    display.setFullWindow();
+    display.firstPage();
+    do
+    {
+        display.fillScreen(GxEPD_WHITE);
+        display.setCursor(x, y);
+        display.print(TEXT);
+    }
+    while (display.nextPage());
+
+    Serial.println("Done");
 }
 
 void loop() {
+
 }

bd5f4b322a23b92dc7c7db673675ff6bc44ae72b
Leander <leander.gerwing@gmail.com>
2025-07-11 01:48:11 +0200

    feat: set up ble server

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEService.h>
+#include <BLEAdvertisedDevice.h>
 #include <GxEPD2_BW.h>
 #include <GxEPD2_3C.h>
 #include <Adafruit_GFX.h>
 #include <Fonts/FreeMonoBold9pt7b.h>
-#include "BLEperipheral.h"
 
 #define EPD_DIN    10
 #define EPD_CLK    9
@@ -16,27 +19,45 @@ GxEPD2_BW<GxEPD2_213_B72, GxEPD2_213_B72::HEIGHT> display(
     GxEPD2_213_B72(SS, EPD_DC, EPD_RST, EPD_BUSY)
 );
 
-#if DEVICE_XIAO_S3
-#define BOARD_NAME "XIAO S3"
-#elif DEVICE_XIAO_S3
-#define BOARD_NAME "XIAO C3"
-#else
-#define BOARD_NAME "Unbekanntes Board"
-#endif
-
-
-BLEPeripheral ble(BOARD_NAME,
-                  "4fafc201-1fb5-459e-8fcc-c5c9c331914b",
-                  "beb5483e-36e1-4688-b7f5-ea07361b26a8");
+#define DEVICE_NAME "IDAC"
 
 
 const char TEXT[] = "Hello world!";
 
+
+class Callbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+    String value = pCharacteristic->getValue();
+    Serial.print("Empfangen: ");
+    Serial.println(value);
+  }
+};
+
+
+void initBLEServer() {
+  BLEDevice::init(DEVICE_NAME);
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService("12345678-1234-5678-1234-56789abcdef0");
+
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+    "abcdefab-1234-5678-1234-56789abcdef0",
+    BLECharacteristic::PROPERTY_WRITE
+  );
+
+  pCharacteristic->setCallbacks(new Callbacks());
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->start();
+
+  Serial.println("BLE server started, waiting for clients.");
+}
+
 void setup() {
-    Serial.begin(115200);
+  Serial.begin(115200);
     delay(1000);
-    // Serial.println("Init bluetooth server...");
-    // ble.begin();
+    Serial.println("Init BLE server...");
+    initBLEServer();
 
     Serial.println("Init display...");
 
@@ -59,11 +80,10 @@ void setup() {
     }
     while (display.nextPage());*/
 
-    Serial.println("Done");
+    Serial.println("Setup done");
 }
 
 void loop() {
-    // ble.sendData("Ping from XIAO");
     Serial.println("Hello world!");
-    delay(1000);
+    delay(5000);
 }
